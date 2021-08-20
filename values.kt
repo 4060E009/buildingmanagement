88f9d0fc441aa6065a63f41bf05c807b77ae9549
4060E009 <37571825+4060E009@users.noreply.github.com>
2021-08-20 17:34:22 +0800

    Create values.kt

---
+++ values.kt
@@ -0,0 +1,73 @@
+colors.xml
+**
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <color name="purple_200">#FFBB86FC</color>
+    <color name="purple_500">#FF6200EE</color>
+    <color name="purple_700">#FF3700B3</color>
+    <color name="teal_200">#FF03DAC5</color>
+    <color name="teal_700">#FF018786</color>
+    <color name="black">#FF000000</color>
+    <color name="white">#FFFFFFFF</color>
+    <color name="loading">#6296FE</color>
+    <color name="Choosecommunity">#8A98B4</color>
+    <color name="buttonbackground">#E0E4EC</color>
+    <color name="forgotresidentcode">#3F414B</color>
+</resources>
+========================================================================
+ strings.xml
+**
+ <resources>
+    <string name="app_name">Building management</string>
+    <string name="loading">歡迎使用 DUC iNSPIRE X</string>
+    <string name="loadingtext">社區服務零時差 隨時掌握新動態</string>
+    <string name="loadingbutton">立即登入</string>
+    <string name="login">登入</string>
+    <string name="Choosecommunity">請選擇社區</string>
+    <string name="Choosecommunity2">請輸入住戶代碼</string>
+    <string name="logintext">忘記住戶代碼?</string>
+    <string name="privacy">登入即表示您同意 DUCTECH的</string>
+    <string name="Policy">隱私權政策</string>
+    <string name="forgotresidentcode">忘記住戶代碼</string>
+    <string name="forgot">若您的 iPad 住戶代碼遺忘，請洽詢</string>
+    <string name="forgot1">社區管理中心協助設定</string>
+    <string name="loading1">正在載入...</string>
+    <string name="Policytext">歡迎您使用 DUC iNSPIRE X 社區管理 App（以下簡稱本App），為了讓您能夠安心的使用本App的各項服務與資訊，特此向您說明本App的隱私權保護政策，以保障您的權益，請您詳閱下列內容：
+
+一、隱私權保護政策的適用範圍
+隱私權保護政策內容，包括本App如何處理在您使用App服務時收集到的個人識別資料。隱私權保護政策不適用於本App以外的相關連結網站，也不適用於非本App所委託或參與管理的人員。
+
+二、個人資料的蒐集、處理及利用方式
+‧ 當您造訪本App或使用本App所提供之功能服務時，我們將視該服務功能性質，請您提供必要的個人資料，並在該特定目的範圍內處理及利用您的個人資料；非經您書面同意，本App不會將個人資料用於其他用途。
+‧ 本App在您使用服務信箱、問卷調查等互動性功能時，會保留您所提供的姓名、電子郵件地址、聯絡方式及使用時間等。
+‧ 於一般瀏覽時，系統會自行記錄相關行徑，包括您使用連線設備的IP位址、使用時間、使用的瀏覽器、瀏覽及點選資料記錄等，做為我們增進App服務的參考依據，此記錄為內部應用，決不對外公佈。
+‧ 為提供精確的服務，我們會將收集的問卷調查內容進行統計與分析，分析結果之統計數據或說明文字呈現，除供內部研究外，我們會視需要公佈統計數據及說明文字，但不涉及特定個人之資料。
+
+三、資料之保護
+‧ 本App主機均設有防火牆、防毒系統等相關的各項資訊安全設備及必要的安全防護措施，加以保護App及您的個人資料採用嚴格的保護措施，只由經過授權的人員才能接觸您的個人資料，相關處理人員皆簽有保密合約，如有違反保密義務者，將會受到相關的法律處分。
+‧ 如因業務需要有必要委託其他單位提供服務時，本App亦會嚴格要求其遵守保密義務，並且採取必要檢查程序以確定其將確實遵守。
+
+四、App對外的相關連結
+‧ 本App內提供其他App及網站的連結，您也可經由本App所提供的連結，點選進入其他App及網站。但該連結不適用本App的隱私權保護政策，您必須參考該其連結App及網站中的隱私權保護政策。
+
+五、與第三人共用個人資料之政策
+‧ 本App絕不會提供、交換、出租或出售任何您的個人資料給其他個人、團體、私人企業或公務機關，但有法律依據或合約義務者，不在此限。
+‧ 前項但書之情形包括不限於：
+⁃ 經由您書面同意。
+⁃ 法律明文規定。
+⁃ 為維護國家安全或增進公共利益。
+⁃ 為免除您生命、身體、自由或財產上之危險。
+⁃ 公務機關或學術研究機構基於 利益為統計或學術研究而有必要，且資料經過提供者處理
+  或蒐集著依其揭露方式無從識 別特定之當事人。
+⁃ 當您在本App的行為，違反本App的服務條款或可能損害或妨礙本App權益或導致任何人
+  遭受損害時，經本App研析揭露您的個人資料是為了辨識、聯絡或採取法律行動所必要
+  者。
+⁃ 有利於您的權益。
+‧ 本App委託廠商協助蒐集、處理或利用您的個人資料時，將對委外廠商或個人善盡監督管理之責。
+
+六、隱私權保護政策之修正
+‧ 本App隱私權保護政策將因應需求隨時進行修正，修正後的條款將更新於本App。
+
+如果我們隱私權條款有任何不清楚的地方或疑問，請聯繫我們 : service@ductech.tw</string>
+
+</resources>
